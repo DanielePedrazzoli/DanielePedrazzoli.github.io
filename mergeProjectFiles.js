efabeb40563ac7cf5644231a2d09f0929f9a3a43
DanielePedrazzoli <danielepedrazzoli2000@gmail.com>
2024-07-05 11:17:35 +0200

    test github action

--- mergeProjectFiles.js
+++ mergeProjectFiles.js
@@ -2,59 +2,6 @@ const fs = require('fs').promises;
 
 
 
-// async function main() {
-//     const mergeContent = {};
-//     const projectPath = "./Projects/";
-//     var avaiableProjectsDirecotry = await fs.readdir(projectPath);
-//     console.log("\nFounded projects:")
-//     for (const project of avaiableProjectsDirecotry) {
-
-//         var stats = {
-//             "name": project,
-//             "description_present": false,
-//             "paragraphs_number": 0,
-//         }
-
-//         var info = JSON.parse(await fs.readFile(projectPath + "/" + project + "/info.json", "utf8"));
-//         var fileContent = await fs.readFile(projectPath + "/" + project + "/desc.md", "utf8");
-
-//         const paragraphs = {};
-//         const sections = fileContent.replace(/\r/gm, "").split(/\n\n#/gm);
-
-//         const description = sections.shift().trim();
-
-//         if (description != "") {
-//             stats.description_present = true;
-//         }
-
-//         sections.forEach(section => {
-//             const lines = section.split('\n\n');
-//             const title = lines.shift().replace('#', '').trim();
-//             const paragraph = lines.join('\n\n').trim();
-//             paragraphs[title] = paragraph;
-
-//             stats.paragraphs_number++;
-//         });
-
-//         console.log(stats);
-
-
-//         mergeContent[project] = {
-//             "info": info,
-//             "description": description,
-//             "paragraphs": paragraphs
-//         }
-
-//     }
-
-//     await fs.writeFile("./database/projects.js", "const projects = " + JSON.stringify(mergeContent, null, 2));
-//     console.log("done");
-
-// }
-
-
-
-
 async function main() {
     const mergeContent = {};
     const projectPath = "./Projects/";
@@ -65,27 +12,6 @@ async function main() {
         console.log(project)
 
         var info = JSON.parse(await fs.readFile(projectPath + "/" + project + "/info.json", "utf8"));
-        // var fileContent = await fs.readFile(projectPath + "/" + project + "/desc.md", "utf8");
-
-        // const paragraphs = {};
-        // const sections = fileContent.replace(/\r/gm, "").split(/\n\n#/gm);
-
-        // const description = sections.shift().trim();
-
-        // if (description != "") {
-        //     stats.description_present = true;
-        // }
-
-        // sections.forEach(section => {
-        //     const lines = section.split('\n\n');
-        //     const title = lines.shift().replace('#', '').trim();
-        //     const paragraph = lines.join('\n\n').trim();
-        //     paragraphs[title] = paragraph;
-
-        //     stats.paragraphs_number++;
-        // });
-
-        // console.log(stats);
 
         info.descriptionLocation = "/" + project + "/" + info.descriptionLocation
 
@@ -97,7 +23,8 @@ async function main() {
 
     }
 
-    await fs.writeFile("./database/projects.js", "const projects = " + JSON.stringify(mergeContent, null, 2));
+    var result = await fs.writeFile("./database/projects.js", "const projects = " + JSON.stringify(mergeContent, null, 2));
+    console.log(result);
     console.log("done");
 
 }
